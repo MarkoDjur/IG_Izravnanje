62575cffa14d0587d6b0aeaabcfdba7b48b8ea73
User <User@DESKTOP-DA422MB>
2023-05-02 22:10:56 +0200

    Racunanje matrice Qx

--- IG_Izravnanje/src/application/MinimalniTrag1D.java
+++ IG_Izravnanje/src/application/MinimalniTrag1D.java
@@ -9,6 +9,11 @@ public class MinimalniTrag1D {
 	private ObservableList<VisinskaRazlika> visinske_razlike;
 	private Matrix matrica_A;
 	private Matrix matrica_P;
+	private Matrix vektor_f;
+	private Matrix matrica_N;
+	private Matrix vektor_n;
+	private Matrix matrica_BT;
+	private Matrix matrica_Qx;
 
 	public MinimalniTrag1D(ObservableList<Visina> visine, ObservableList<VisinskaRazlika> visinske_razlike) {
 		this.visine = visine;
@@ -33,6 +38,7 @@ public class MinimalniTrag1D {
 									visine.get(i).setVisina(
 											Double.toString(Double.parseDouble(poznate_visine.get(k).getVisina())
 													+ Double.parseDouble(visinske_razlike.get(j).getVisinskaRaz())));
+									break;
 								}
 							}
 						} catch (Exception e) {
@@ -45,6 +51,7 @@ public class MinimalniTrag1D {
 									visine.get(i).setVisina(
 											Double.toString(Double.parseDouble(poznate_visine.get(k).getVisina())
 													- Double.parseDouble(visinske_razlike.get(j).getVisinskaRaz())));
+									break;
 								}
 							}
 						} catch (Exception e) {
@@ -57,6 +64,11 @@ public class MinimalniTrag1D {
 
 		formirajMatricuA();
 		formirajMatricuP();
+		formirajVektorf();
+		izracunajMatricuN();
+		izracunajVektorn();
+		formirajMatricuBT();
+		izracunajMatricuQx();
 
 	}
 
@@ -113,6 +125,60 @@ public class MinimalniTrag1D {
 		}
 		
 		matrica_P = new Matrix(niz);
+	}
+	
+	private void formirajVektorf() {
+		int n = visinske_razlike.size();
+		double niz[][] = new double[n][1];
+		
+		for(int i = 0; i < n; i++) {
+			double visinska_razlika = Double.parseDouble(visinske_razlike.get(i).getVisinskaRaz());
+			String OD = visinske_razlike.get(i).getOd();
+			String DO = visinske_razlike.get(i).getDo();
+			niz[i][0] = (nadjiVisinu(DO) - nadjiVisinu(OD) - visinska_razlika) * 1000;
+		}
+		
+		vektor_f = new Matrix(niz);
+		
+	}
+	
+	private void izracunajMatricuN() {
+		matrica_N = matrica_A.multiply(matrica_A.transpose()).multiply(matrica_P).multiply(matrica_A);
+	}
+	
+	private void izracunajVektorn() {
+		vektor_n = matrica_A.multiply(matrica_A.transpose()).multiply(matrica_P).multiply(vektor_f);
+	}
+	
+	private void formirajMatricuBT() {
+		int u = visine.size();
+		double niz[][] = new double[1][u];
+		
+		for(int i = 0; i < u; i++) {
+			niz[0][i] = 1;
+		}
+		
+		matrica_BT = new Matrix(niz);
+	}
+	
+	private void izracunajMatricuQx() {
+		Matrix BxBT = matrica_BT.transpose().multiply(matrica_BT);
+		matrica_Qx = (matrica_N.add(BxBT).inverse()).subtract(BxBT);
+		System.out.println(matrica_Qx);
+	}
+	
+	private double nadjiVisinu(String oznaka) {
+		String v = "";
+		for(int i = 0; i < visine.size(); i++) {
+			if(visine.get(i).getOznaka().equals(oznaka)) {
+				v = visine.get(i).getOznaka();
+				break;
+			}
+		}
+		
+		double visina = Double.parseDouble(v);
+		
+		return visina;
 	}
 
 }
